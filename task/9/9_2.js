66ccd9e8d995642fcbfb20cb3b9e16b6635b67e6
Amith P <amithanilp@gmail.com>
2025-07-29 21:48:06 +0530

    task 9.2 complete

---
+++ task/9/9_2.js
@@ -0,0 +1,20 @@
+let a=5
+const b=5
+console.log(a,b)
+a=6
+// b=4 return error,cont variable value cant be reassigned
+console.log(a,b)
+
+let c="amith"
+let d=true
+let e=[1,2,3]
+let f={1:"one",2:"two"}
+console.log(a,typeof(a))
+console.log(c,typeof(c))
+console.log(d,typeof(d))
+console.log(e,typeof(e))
+console.log(f,typeof(f))
+
+console.log(a+b)
+console.log(a==b)
+console.log(a&&b)
