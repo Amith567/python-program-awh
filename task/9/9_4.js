0149c40da6fd74ccc63b1af33d8e818f6751d350
Amith P <amithanilp@gmail.com>
2025-07-29 22:06:21 +0530

    task 9.4 complete

---
+++ task/9/9_4.js
@@ -0,0 +1,12 @@
+// Task 9.4: Functions
+
+// ✔ Create a function greetUser(name) that prints:
+// Hello, [name]! Welcome to Engineers Squad.
+
+// ✔ Call the function with different names
+
+function greetuser(name){
+    console.log(`hello ${name} ! welcome to engineers squad.`)
+}
+greetuser('amith')
+greetuser("user")
